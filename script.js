6e58579952bc9cbb4889c9bbbd9067e083a6eceb
Nick <Nick_II2000@mail.ru>
2022-06-16 08:39:35 +0600

    10-th commit

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ function User(name, id) {
     this.name = name;
     this.id = id;
     this.human = true;
+    this.hello = function () {
+        console.log(`Hello, ${this.name}`);
+    };
 }
 
 const ivan = new User('Ivan', 28);
@@ -32,3 +35,6 @@ const alex = new User('Alex', 20);
 
 console.log(ivan);
 console.log(alex);
+
+ivan.hello();
+alex.hello();
