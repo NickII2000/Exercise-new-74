78d5e1257ff31252ff89940904e2c648789109b4
Nick <Nick_II2000@mail.ru>
2022-06-16 08:17:08 +0600

    6-th commit

--- script.js
+++ script.js
@@ -15,5 +15,17 @@ observer.observe(box, {
 
 // 75. Функции-конструкторы
 
-const num = new Number(3);
-console.log(num);
+// const num = new Number(3);
+// console.log(num);
+
+// const fun = new Function(3);
+// console.log(fun);
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+}
+
+const ivan = new User('Ivan', 28);
+console.log(ivan);
