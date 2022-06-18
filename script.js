fe02fc6f447b7a82993b73f21db8b15ccc170d3d
Nick <Nick_II2000@mail.ru>
2022-06-18 15:32:11 +0600

    44-th commit

--- script.js
+++ script.js
@@ -110,6 +110,7 @@ const double = count.bind(2);
 console.log(double(100));
 console.log(double(13));
 
+
 // 1) Обычная функция: this = window, но если 'use strict' - undefined.
 // 2) Контекст у методов объекта - сам объект.
 // 3) This в конструкторах и классах = это новый экземпляр объекта.
