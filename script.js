f22720aaf3beec34f9c3c49e6fc34db550ed38b6
Nick <Nick_II2000@mail.ru>
2022-06-18 18:04:57 +0600

    56-th commit

--- script.js
+++ script.js
@@ -156,8 +156,24 @@ class Rectangle {
     }
 }
 
-const square = new Rectangle(10, 10);
-const long = new Rectangle(20, 100);
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
 
-console.log(square.calcArea());
-console.log(long.calcArea());
+    showMyProps() {
+        console.log(`Текст: ${this.text}, ${this.bgColor}`);
+    }
+
+}
+
+const div = new ColoredRectangleWithText(25, 20, 'Hello World', 'red');
+div.showMyProps();
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
