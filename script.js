96b276f817d9683b337965913fef5ad4d3c8998a
Nick <Nick_II2000@mail.ru>
2022-06-18 14:33:11 +0600

    28-th commit

--- script.js
+++ script.js
@@ -58,10 +58,11 @@ alex.hello();
 function showThis(a, b) {
     console.log(this);
     function sum() {
+        console.log(this);
         return this.a + this.b;
     }
-
+    console.log(sum());
 }
-showThis();
+showThis(4, 5);
 
 // 1) Обычная функция: this = window, но если 'use strict' - undefined.
